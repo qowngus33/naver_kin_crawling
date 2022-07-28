016533d9fdb604f5135f7dc33ac3fa5ced69426d
qowngus33 <qowngus33@naver.com>
2022-07-28 22:32:56 +0900

    change driver.find_element_by_class_name to post_number = driver.find_element(By.CLASS_NAME)

--- project.py
+++ project.py
@@ -9,19 +9,25 @@ from openpyxl import Workbook
 from random import *
 
 # firefox 버전
+from selenium.webdriver.common.by import By
+
 profile = webdriver.FirefoxProfile()
-profile.set_preference('general.useragent.override', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0')
+profile.set_preference('general.useragent.override',
+                       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0')
 profile.set_preference("network.proxy.type", 1)
 profile.set_preference("network.proxy.socks", "127.0.0.1")
 profile.set_preference("network.proxy.socks_port", 9050)
 
-path = "/Users/taehyung/anaconda3/envs/study/geckodriver"
+# geckodriver가 위치한 경로
+path = "/Users/baejuhyeon/Downloads/geckodriver"
 driver = webdriver.Firefox(firefox_profile=profile, executable_path=path)
+
 
 # 네이버 지식인 크롤링
 # keyword에 크롤링하고 싶은 단어 선택. space 는 + 로 치환
 def get_keyword(text):
     return text.replace(" ", "%20")
+
 
 # 정렬 방식 선택
 # 1: 추천순
@@ -38,19 +44,18 @@ def sort_kind(index):
     else:
         return 'none'
 
-
-keyword = '자살 자해'
+keyword = '개 동물의료상담'
 driver.get('https://kin.naver.com/search/list.nhn?query=' + get_keyword(keyword))
 time.sleep(uniform(0.1, 1.0))
 
 page_index = 1
 # 크롤링 시작 일자
-f = '2019.01.01'
+f = '2018.01.01'
 # 크롤링 종료 일자
-t = '2020.02.06'
+t = '2022.07.27'
 period_txt = "&period=" + f + ".%7C" + t + "."
 
-_sort_kind = sort_kind(2)
+_sort_kind = sort_kind(1)
 date = str(datetime.now()).replace('.', '_')
 date = date.replace(' ', '_')
 
@@ -59,7 +64,8 @@ f = open("result/url_list" + "_" + keyword.replace(' ', '+') + "_" + date + ".tx
 page_url = []
 while True:
     time.sleep(uniform(0.01, 1.0))
-    driver.get('https://kin.naver.com/search/list.nhn?' + "&sort=" + _sort_kind + '&query=' + get_keyword(keyword) + period_txt + "&section=kin" + "&page=" + str(page_index))
+    driver.get('https://kin.naver.com/search/list.nhn?' + "&sort=" + _sort_kind + '&query=' + get_keyword(
+        keyword) + period_txt + "&section=kin" + "&page=" + str(page_index))
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -72,10 +78,11 @@ while True:
         page_url.append(url)
         f.write(url + "\n")
 
-    post_number = driver.find_element_by_class_name('number').text
+    # post_number = driver.find_element_by_class_name('number').text
+    post_number = driver.find_element(By.CLASS_NAME,'number').text
     post_number = str(post_number).replace("(", "")
     post_number = str(post_number).replace(")", "")
-    
+
     current_number = post_number.split('/')[0].split('-')[1]
     current_number = current_number.replace(',', '')
     total_number = post_number.split('/')[1]
@@ -90,34 +97,29 @@ filename = 'result/' + keyword.replace(' ', '.') + "_" + date + "_crawling_resul
 
 wb = Workbook()
 sheet = wb.active
-sheet.append(['제목', '질문', '답변'])
+sheet.append(['제목','질문'])
 
 for j in range(1, 4):
     sheet.cell(row=1, column=j).fill = PatternFill(start_color='808080', end_color='808080', fill_type='solid')
 
+count = 0
 for i in page_url:
     driver.get(i)
-    title = driver.find_element_by_class_name('title').text
+    # title = driver.find_element_by_class_name('title').text
+
     try:
-        question_txt = driver.find_element_by_class_name('c-heading__content').text
-        
+        title = driver.find_element(By.CLASS_NAME,'title').text
+    except:
+        title = ""
+
+    try:
+        question_txt = driver.find_element(By.CLASS_NAME,'c-heading__content').text
     except:
         question_txt = ""
 
     # 답변 리스트
-    answer_list = driver.find_elements_by_class_name("se-main-container")
-    
-    for n, answer in enumerate(answer_list):
-        texts = answer.find_elements_by_tag_name('span')
-        t = ""
-        for i in texts:
-            t += i.text
-
-        if n == 0:
-            sheet.append([title, question_txt, t])
-        else:
-            sheet.append(["", "", t])
-    
+    # answer_list = driver.find_elements_by_class_name("se-main-container")
+    sheet.append([title, question_txt])
     count += 1
     print(count)
     wb.save(filename)
